c20dfd8be51ca7317701f43eb50f4125aae1f5f6
gitXite <daniel.halaas@gmail.com>
2025-06-15 17:28:02 +0200

    Added logo

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import path from "path";
 const app = express();
 const PORT = 8080;
 
-app.use(express.static(".src"));
+app.use(express.static("."));
+app.use(express.static("assets"));
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
